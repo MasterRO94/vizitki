f9b56d856dfb8131b4248755da585a5240325278
MasterRO <igoshin18@gmail.com>
2014-09-19 16:26:04 +0300

    'file_upload'

--- controllers/controller.php
+++ controllers/controller.php
@@ -18,6 +18,50 @@
     $editor = false;
 
 
+    /*------------------------------------------
+    // UPLOAD IMAGE TO TEMP FOLDER FOR EDITOR //
+    -------------------------------------------*/
+        function uploadImage(){
+            $error = false;
+            if(empty($_FILES)){
+                return false;
+            }
+            $type = explode('/', $_FILES['file']['type']);
+            if($type[0] != 'image'){
+                $result = 'Not image!';
+                $error = true;
+            }
+            if($_FILES['file']['size'] > 2000000){
+                $result = 'Too large!';
+                $error = true;
+            }
+
+            if(!$error){
+                $uploadDir = 'uploads/_temp/';
+                $uploadImage = $uploadDir . uniqid(true) .'.'. $type[1];
+                $result = array('tmp_name' => $_FILES['file']['tmp_name'], 'uploadDir' => $uploadDir, 'uploadImage' => $uploadImage);
+                $success = is_uploaded_file($_FILES['file']['tmp_name']);
+
+               /* print_arr($_FILES);
+                var_dump($result);
+                die;*/
+
+                if($success){
+                    move_uploaded_file($_FILES['file']['tmp_name'], $uploadImage);
+
+                    $result[] = '<script type="text/javascript">
+                                 $("body").empty();
+                                  var photo_name = "' .$uploadImage. '";
+                                  window.parent.$("#new_photo").html(photo_name);
+                                </script>';
+                }else{
+                    $result[] = 'File not uploaded!';
+                }
+
+            }
+            return $result;
+        }
+    /*-------------------------------------------*/
 
 
 
@@ -55,7 +99,9 @@
         // uploadImage
         case('uploadImageToEditor'):
             if(isset($_POST['uploadImage'])){
-               uploadImage();
+               echo uploadImage();
+               var_dump(uploadImage());
+               print_arr($_FILES);
             }else{
                 redirect();
             }
